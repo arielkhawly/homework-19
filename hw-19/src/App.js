9fadce66ec03fcebb0ed93b94a0a3a1691c539ad
Ariel Khawly <akhawly59@gmail.com>
2020-03-24 13:01:25 -0700

    table creation

---
+++ hw-19/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import UserTable from './components/UserTable';
+import './App.css';
+
+function App() {
+  return (
+   <UserTable  /> 
+  );
+}
+
+export default App;
---
+++ hw-19/src/components/UserTable.js
@@ -0,0 +1,34 @@
+import React, {Fragment} from "react";
+
+function UserTable() {
+    return(
+        <table>
+  <tr>
+    <th>First Name</th>
+    <th>Last Name</th> 
+    <th>Role</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td> 
+    <td>Developer</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td> 
+    <td>Engineer</td>
+  </tr>
+  <tr>
+    <td>Ariel</td>
+    <td>Khawly</td> 
+    <td>Developer</td>
+  </tr>
+  <tr>
+    <td>Jane</td>
+    <td>Lane</td> 
+    <td>Engineer</td>
+  </tr>
+</table>
+    )
+}
+export default UserTable;
